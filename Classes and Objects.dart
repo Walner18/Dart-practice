76c6177eebe8db02e2b165e04aaaaf85405eb827
Walner18 <128760414+Walner18@users.noreply.github.com>
2024-10-16 09:50:59 -0400

    Create Classes and Objects.dart
    Classes and Objects.

---
+++ Classes and Objects.dart
@@ -0,0 +1,30 @@
+class Person {
+    String name;
+    int age;
+
+    Person({required this.name, required this.age});
+
+    void showDetails() {
+        print('Name: $name');
+        print('Age: $age');
+    }
+}
+
+class Employee extends Person {
+    String position;
+
+    Employee({required String name, required int age, required this.position})
+        : super(name: name, age: age);
+
+    @override
+    void showDetails() {
+        super.showDetails();
+        print('Position: $position');
+    }
+}
+
+void main() {
+    Employee employee = Employee(name: 'Camila', age: 23, position: 'DBA');
+
+    employee.showDetails();
+}
